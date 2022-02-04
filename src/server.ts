d7005ae2dfbb8610aea805448a1322ead389e42c
deboramartins15 <debora_martins98@hotmail.com>
2022-02-04 12:51:55 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+
+//import "./database";
+import { routes } from "../routes";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3001, () => console.log("🚀 Server is running on 3001"));
