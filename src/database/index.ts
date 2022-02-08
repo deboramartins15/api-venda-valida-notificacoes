8cad861c68ce10114431626c8e33852e511f4847
deboramartins15 <debora_martins98@hotmail.com>
2022-02-08 18:28:23 -0300

    [New] Ajuste para o build

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,15 @@
 import { createConnection } from "typeorm";
+import "dotenv/config";
+import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
 
-createConnection();
+const conn: MysqlConnectionOptions = {
+  type: "mysql",
+  host: process.env.TYPEORM_HOST,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  port: 3306,
+  entities: ["src/entities/*.ts", "./build/src/entities/*.js"],
+};
+
+createConnection(conn);
